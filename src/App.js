440d0776dc3762e84c75efd3d8aa43490b3fc75e
Adolfo Gonzalez <agonzal177@gmail.com>
2022-04-18 05:37:34 -0400

    added works component and component starter files for future development

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
+import React from "react";
+import Portfolio from "./portfolio";
+import Navbar from "./components/navbar/navbar"
 
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Portfolio />;
+    </div>
+  );
+}
+
+export default App;
