49d67bf59a21afa93861c44ca50c2c2e524495df
Adolfo Gonzalez <agonzal177@gmail.com>
2022-04-19 05:14:01 -0400

    added styling to cards

--- src/components/works/works.js
+++ src/components/works/works.js
@@ -6,71 +6,70 @@ const projects = [
         description: "This application which is live on heroku is a demo music shop. It contains products to purchase, a logIn LogOut feature, a user cart, and an email confirmation once finished shopping.",
         contributers: "Contributers: Adolfo Gonzalez, Brittany Klose, Sam Meske",
         technologies: "Technologies Used: Node.js, Express, Handlebars, Node Mailer, Bootstrap",
-        deployedAt:"https://mortal-instruments-music-shop.herokuapp.com/",
-        repo:"https://github.com/smeske10/Electric-Wipeouts-Mortal-Instruments",
+        deployedAt: "https://mortal-instruments-music-shop.herokuapp.com/",
+        repo: "https://github.com/smeske10/Electric-Wipeouts-Mortal-Instruments",
     },
     {
         title: "Weather Forecast",
         description: "This application will provide you with the temperature of a given city along with a 5 day forecast.",
         contributers: "Contributers: Adolfo Gonzalez",
         technologies: "Technologies Used: HTML, CSS JavaScript, Moment API, OpenWeatherMap API",
-        deployedAt:"https://adolfo-g.github.io/Weather-Forecast/",
-        repo:"https://github.com/Adolfo-G/Weather-Forecast",
+        deployedAt: "https://adolfo-g.github.io/Weather-Forecast/",
+        repo: "https://github.com/Adolfo-G/Weather-Forecast",
     },
     {
         title: "Countdown Games!",
         description: "The following is a trivia quiz which can be replayed to try and increase your personal score.",
         contributers: "Contributers: Adolfo Gonzalez, Bowen Zheng, Maria Gonzalez, Caprice Dutton",
-        technologies: "Technologies Used: HTML, CSS, JavaScript, FontAwesome API, GoogleFonts API, OpenTriviaDB API, RandomWords API, Foundation Framework",
-        deployedAt:"https://adolfo-g.github.io/Countdown-Games/",
-        repo:"https://github.com/Adolfo-G/Countdown-Games",
+        technologies: "Technologies Used: JavaScript, GoogleFonts API, OpenTriviaDB API, RandomWords API, Foundation Framework",
+        deployedAt: "https://adolfo-g.github.io/Countdown-Games/",
+        repo: "https://github.com/Adolfo-G/Countdown-Games",
     },
 ]
 
 function Works() {
     return (
         <div>
-            <div className="grid-container">
-                <div className="grid-x grid-margin-x">
-                    <div className="cell">
-                        <h4 id="works">Works</h4>
-                        <div className="card">
-                            <img className="preview-img" src="assets/images/music-shop.jpeg" />
-                            <div className="card-section">
-                                <h4>{projects[0].title}</h4>
-                                <p>{projects[0].description}</p>
-                                <p>{projects[0].contributers}</p>
-                                <p>{projects[0].technologies}</p>
-                                <p>Deployed App: <a href={projects[0].deployedAt}>Click here</a> Github Repo: <a href={projects[0].repo}>Click here</a></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
+            <h4 id="works">Works</h4>
             <div className="grid-container">
                 <div className="grid-x grid-margin-x small-up-1 medium-up-2">
                     <div className="cell">
                         <div className="card">
-                            <img className="preview-img" src="assets/images/weather-preview.jpeg" />
-                            <div className="card-section">
-                                <h4>{projects[1].title}</h4>
-                                <p>{projects[1].description}</p>
-                                <p>{projects[1].contributers}</p>
-                                <p>{projects[1].technologies}</p>
-                                <p>Deployed App: <a href={projects[1].deployedAt}>Click here</a> Github Repo: <a href={projects[1].repo}>Click here</a></p>
+                            <div className="image-effect">
+                                <img src="assets/images/music-shop.jpeg" />
+                            </div>
+                            <div className='text-effect'>
+                                <div className="text">
+                                    <h4>{projects[0].title}</h4>
+                                    <p>Deployed App: <a href={projects[0].deployedAt}>Click here</a> Github Repo: <a href={projects[0].repo}>Click here</a></p>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="cell">
                         <div className="card">
-                            <img className="preview-img" src="assets/images/TriviaHomePage.png" />
-                            <div className="card-section">
-                                <h4>{projects[2].title}</h4>
-                                <p>{projects[2].description}</p>
-                                <p>{projects[2].contributers}</p>
-                                <p>{projects[2].technologies}</p>
-                                <p>Deployed App: <a href={projects[2].deployedAt}>Click here</a> Github Repo: <a href={projects[2].repo}>Click here</a></p>
+                            <div className="image-effect">
+                                <img src="assets/images/weather-preview.jpeg" />
+                            </div>
+                            <div className='text-effect'>
+                                <div className="text">
+                                    <h4>{projects[1].title}</h4>
+                                    <p>{projects[1].technologies}</p>
+                                    <p>Deployed App: <a href={projects[1].deployedAt}>Click here</a> Github Repo: <a href={projects[1].repo}>Click here</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="cell">
+                        <div className="card">
+                            <div className="image-effect">
+                                <img src="assets/images/TriviaHomePage.png" />
+                            </div>
+                            <div className='text-effect'>
+                                <div className="text">
+                                    <h4>{projects[2].title}</h4>
+                                    <p>Deployed App: <a href={projects[2].deployedAt}>Click here</a> Github Repo: <a href={projects[2].repo}>Click here</a></p>
+                                </div>
                             </div>
                         </div>
                     </div>
