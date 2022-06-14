c62d0a235b6a6ed7b61bf6a88d0aae986f4987d8
Adolfo Gonzalez <agonzal177@gmail.com>
2022-06-14 15:36:03 -0400

    updated about me and resume

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -10,9 +10,9 @@ function About() {
         </div>
         <div className="media-object-section">
           <h4 id="about-me">About Me</h4>
-          <p> I am currently a full stack bootcamp student at the University of Pennsylvania.
+          <p> I am a recent full stack bootcamp graduate from the University of Pennsylvania.
             I am heavily engaged in learning HTML, CSS, and JavaScript.
-            I am currently using my interest in computer coding to help me through a career transition.
+            I am currently using my interest in web development to help me through a career transition.
             Computer coding has been an interest of mine for many years but only now have I chosen to pursue it.
             Please take a look at some of the projects I have worked on!</p>
         </div>
