2738a703ca6f509662fd8024652d27e4ee66ac0a
Adolfo Gonzalez <agonzal177@gmail.com>
2023-02-02 15:02:06 -0500

    update to about me

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -10,10 +10,10 @@ function About() {
         </div>
         <div className="media-object-section">
           <h4 id="about-me">About Me</h4>
-          <p className='aboutMe-txt'> I am a recent full stack bootcamp graduate from the University of Pennsylvania.
-            I am heavily engaged in learning HTML, CSS, and JavaScript.
+          <p className='aboutMe-txt'> Hello! I am a full stack bootcamp graduate from the University of Pennsylvania.
+            Since graduating I have had the opportunity to work on my skill's via projects using a MERN stack as well as attending a hack-a-thon.
             I am currently using my interest in web development to help me through a career transition.
-            Computer coding has been an interest of mine for many years but only now have I chosen to pursue it.
+            Computer programming has been an interest of mine for many years and I am actively looking for a developer position in this exciting industry.
             Please take a look at some of the projects I have worked on!</p>
         </div>
       </div>
